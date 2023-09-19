e40ad461c85737c62c4792c9aae4d1ab137205b3
Timo Schmidt <tischmid@student.42berlin.de>
2023-09-20 01:33:39 +0200

    Kickstart configuration and make_patch_image to generate patch image for kickstart configuration

---
+++ make_patch_image.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd -P "$(dirname -- "${0}")"
+
+if [ ! -x './python2/bin/python2' ]; then
+	wget https://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz -O ./Python-2.7.5.tgz
+	tar xf ./Python-2.7.5.tgz
+	rm -rf ./Python-2.7.5.tgz
+	pushd ./Python-2.7.5
+	./configure prefix="${PWD}/../python2"
+	make install
+	popd
+	rm -rf ./Python-2.7.5
+fi
+
+if [ ! -x './pigz/pigz' ]; then
+	git clone --depth 1 'https://github.com/madler/pigz' pigz
+	pushd pigz
+	make
+	popd
+fi
+
+rm -rf anaconda
+git clone --depth 1 'https://github.com/rhinstaller/anaconda' -b 'f31-release' anaconda
+pushd anaconda
+sed -i \
+	-e 's/self.add_check(verify_unlocked_devices_have_key)/#self.add_check(verify_unlocked_devices_have_key)/' \
+	'./pyanaconda/storage/checker.py'
+../python2/bin/python2 ./scripts/makeupdates
+popd
+
+cp ./anaconda/updates.img ./kickstart/updates.img
+printf '\033[32mDone, no errors\033[m\n'
